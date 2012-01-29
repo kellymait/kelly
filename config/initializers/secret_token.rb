d14591f2e34064ba2a943ceebfecbd3c5230c662
kailash kumar <kailashk06@gmail.com>
2012-01-29 17:11:23 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kelly::Application.config.secret_token = 'bfcd4ce3c79ee9b9d5d8c3a6791b662d6105c94b854d01e7aa642cf400b0764fbde2d27261168dea498349e26682c0e2aa033eb138e9df3666e10bf73973645a'
